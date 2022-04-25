3f49182c63644a4bf4cb8d7329d29e6825865278
unknown <ryancare@protonmail.com>
2022-04-25 18:16:06 -0500

    Added fake episode data and tested episode component

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Episode from './../Episode';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Episode from "./../Episode";
 
-test("renders without error", () => { });
+const fakeData = {
+  id: 1000000,
+  name: "testEpisode",
+  season: 3,
+  number: 22,
+  summary: "This is the summary",
+  runtime: "Eternal",
+};
 
-test("renders the summary test passed as prop", () => { });
+test("renders without error", () => {
+  render(<Episode episode={fakeData} />);
+});
 
-test("renders default image when image is not defined", () => { });
+test("renders the summary test passed as prop", () => {
+
+    render(<Episode episode={fakeData} />);
+expect(screen.getByText(/This is the summary/)).toBeVisible();
+});
+
+test("renders default image when image is not defined", () => {
+    render(<Episode episode={fakeData} />);
+    expect(screen.getByAltText("https://i.ibb.co/2FsfXqM/stranger-things.png")).toBeInTheDocument();
+});
